74fdfc45c9aeb74bbe104264398f4374d1f57b41
anapp1515 <55018109+anapp1515@users.noreply.github.com>
2022-10-22 11:00:23 +0700

    step awal
    file inisiasi

---
+++ MainActivity.java
@@ -0,0 +1,99 @@
+package com.autotesis.akusaha;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.print.PrintAttributes;
+import android.print.PrintDocumentAdapter;
+import android.print.PrintManager;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    private long pressedTime;
+    private FloatingActionButton fab;
+    @SuppressLint("RestrictedApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fab = findViewById(R.id.fab);
+        final WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.getSettings().setLoadsImagesAutomatically(true);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setDomStorageEnabled(true);
+        myWebView.getSettings().setSupportZoom(false);
+        myWebView.getSettings().setBuiltInZoomControls(true);
+        myWebView.getSettings().setDisplayZoomControls(false);
+        // Baris di bawah untuk menambahkan scrollbar di dalam WebView-nya
+        myWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        myWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                if(myWebView.getUrl().toString().contains("laporan")) {
+                    fab.setVisibility(View.VISIBLE);
+                } else {
+                    fab.setVisibility(View.GONE);
+                }
+            }
+        });
+        myWebView.loadUrl("https://akusaha.autotesis.com");
+        fab.setVisibility(View.GONE);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Preview Dokumen", Toast.LENGTH_SHORT).show();
+                createWebPrintJob(myWebView);
+            }
+        });
+    }
+    /**private void printWebView() {
+        WebView webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return false;
+            }
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                createWebPrintJob(view);
+                myWebView = null;
+            }
+        });
+        String htmlDocument =
+                "<html><body><h1>Android Print Test</h1><p>"
+                        + "This is some sample content.</p></body></html>";
+        webView.loadDataWithBaseURL(null, htmlDocument,
+                "text/HTML", "UTF-8", null);
+        myWebView = webView;
+    }**/
+    private void createWebPrintJob(WebView webView) {
+
+        PrintManager printManager = (PrintManager) this
+                .getSystemService(Context.PRINT_SERVICE);
+
+        PrintDocumentAdapter printAdapter =
+                webView.createPrintDocumentAdapter("LaporanAKUSaha");
+
+        String jobName = getString(R.string.app_name) + " Print Laporan AKUSaha";
+
+        printManager.print(jobName, printAdapter,
+                new PrintAttributes.Builder().build());
+    }
+    @Override
+    public void onBackPressed() {
+
+        if (pressedTime + 2000 > System.currentTimeMillis()) {
+            super.onBackPressed();
+            finish();
+        } else {
+            Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
+        }
+        pressedTime = System.currentTimeMillis();
+    }
+}
